4671c311113d3521632a73603480a28bf43c5ac8
BrcRs <47972925+BrcRs@users.noreply.github.com>
2020-02-20 15:51:45 +0100

    UPMC - (20/02/2020)
    En cours de combinatoire 
    2, 3, 4 +/- ok !

--- Projet1/naval.py
+++ Projet1/naval.py
@@ -62,9 +62,12 @@ def place_alea(grille, bateau) :
     y = randint(0, 9)
     d = randint(1,2)
     while not (peut_placer(grille, bateau, (x,y), d)) :
+        # print(".", end="")
         x = randint(0, 9)
         y = randint(0, 9)
         d = randint(1,2)
+        # print("Placer " + str(bateau) + " en " + str((x, y)) + " avec d = " + str(d) + " dans \n" + affiche_mat(grille) + "\n ?")
+        # time.sleep(0.5)
     place(grille, bateau, (x,y), d)
 
 def affiche(grille) :
@@ -94,6 +97,18 @@ def genere_grille():
         place_alea(matrice, i)
     return matrice
 
+def genere_grille_list(listNum):
+    """ list(int) -> list(list(int))
+    Rend une grille avec les bateaux disposés de manière aléatoire
+    """
+    matrice = [[0 for y in range(10)] for x in range(10)]
+    for i in listNum:
+        # if len(listNum) == 1 :
+            # print(i)
+            # time.sleep(0.001)
+        place_alea(matrice, i)
+    return matrice
+
 def genere_grille_vide() :
     """ void -> list(list(int))
     Renvoie une grille vide
@@ -117,18 +132,19 @@ def pos_des_bateauxrec(grille, liste, cpt) :
     """ list(list(int)) * list(int) * int -> int
     Rend le nombre de possibilité de placer des bateaux dans une grille
     """
-    print("=- APPEL pos_des_bateauxrec -=\navec :\ngrille :\n"+affiche_mat(grille)+"\nliste :\n"+str(liste)+"\ncpt :\n"+str(cpt))
-    time.sleep(0.0125)
+    # print("=- APPEL pos_des_bateauxrec -=\navec :\ngrille :\n"+affiche_mat(grille)+"\nliste :\n"+str(liste)+"\ncpt :\n"+str(cpt))
+    # time.sleep(0.005125)
     if len(liste) == 0 :
         return 1
     else :
         for i in range(10):
             for j in range(10):
                 if (peut_placer(grille, liste[0], (i,j), 1)):
-                    cpt += pos_des_bateauxrec(place(copyMat(grille), liste[0], (i,j), 1), liste[1:], cpt)
+                    cpt += pos_des_bateauxrec(place(copyMat(grille), liste[0], (i,j), 1), liste[1:], 0)
                 if (peut_placer(grille, liste[0], (i,j), 2)):
-                    cpt += pos_des_bateauxrec(place(copyMat(grille), liste[0], (i,j), 2), liste[1:], cpt)
-        time.sleep(2)
+                    cpt += pos_des_bateauxrec(place(copyMat(grille), liste[0], (i,j), 2), liste[1:], 0)
+        # print("return cpt =", cpt)
+        # time.sleep(1)
         return cpt
 
 def copyMat(grille) :
@@ -155,7 +171,52 @@ def affiche_mat(grille) :
                 string += str(grille[i][j]) + "\t"
         string += "\n"
     return string
-### TESTS
+
+def grilleAleaEgale(grille, listNum) :
+    """ list(list(int)) * list(int) -> int
+    Prend en paramètre une grille, génère des grilles aléatoirement jusqu’à ce
+    que la grille généréesoit égale à la grille passée en paramètre et qui
+    renvoie le nombre de grilles générées.
+    """
+    grilleGeneree = genere_grille_list(listNum)
+    cpt = 1
+    # print(affiche_mat(grille))
+    # print()
+    # print(affiche_mat(grilleGeneree))
+    # print("=" * 60)
+    while not eq(grille, grilleGeneree) :
+        grilleGeneree = genere_grille_list(listNum)
+        cpt += 1
+        # print("\n" * 2)
+        # print(affiche_mat(grille))
+        # print("- " * 30)
+        # print(affiche_mat(grilleGeneree))
+        # print("=" * 60)
+        # time.sleep(0.01)
+
+    return cpt
+
+def test_grilleAleaEgale(sec, listNum) :
+    print()
+    print("Test grilleAleaEgale ("+str(sec)+"s, "+ str(listNum) +")")
+    cpt = 0
+    nb = 1
+    res = 0
+    start_time = time.clock()
+    end_time = 0
+    while (True) :
+        end_time = time.clock()
+        if (end_time - start_time > sec) :
+            break
+        cpt += grilleAleaEgale(genere_grille_list(listNum), listNum)
+        nb += 1
+    res = (cpt // nb)
+    print("Moyenne pour grilleAleaEgale avec "+ str(listNum) +" : " + str(res))
+    print("(" + str(nb) + " itérations)")
+    print("("+ str(round(end_time - start_time)) +"s)")
+    return res
+
+## TESTS
 
 print(peut_placer(matricetest, 1, (0, 0), 1))
 print(peut_placer(matricetest, 1, (9, 9), 1))
@@ -167,5 +228,19 @@ print(peut_placer(matricetest, 2, (0, 9), 1))
 print(pos_un_bateau(genere_grille_vide(), 5))
 
 print(pos_des_bateaux([]))
-print("Test pos_des_bateaux")
+print("Test pos_des_bateaux [5, 4]")
 print(pos_des_bateaux([5,4]))
+print("Test pos_des_bateaux [5]")
+print(pos_des_bateaux([5]))
+print("Test pos_des_bateaux [0, 0]")
+print(pos_des_bateaux([0, 0]))
+print("Test pos_des_bateaux [5, 4, 3]")
+print("Test pos_des_bateaux [4]")
+print(pos_des_bateaux([4]))
+
+# print(pos_des_bateaux([5, 4, 3])) # Trop long
+
+test_grilleAleaEgale(15, [5])
+test_grilleAleaEgale(15, [5, 4])
+test_grilleAleaEgale(15, [0, 0])
+test_grilleAleaEgale(15, [4])
